363d8c4a6cf3d9cf84e0e28db76ee2fa63c48b85
Lexicon Student 2 <Lexicon Student 2@DESKTOP-2GQ78GN>
2022-05-23 13:48:47 +0200

    uppdaterd

--- Arv/Program.cs
+++ Arv/Program.cs
@@ -8,12 +8,16 @@ dog.age = 4;
 Console.WriteLine("Hunden ålder: "+dog.GetAge());
 
 //3.4
-
 List <Animal> anmimals= new List<Animal>();
 anmimals.Add(new Dog());
 anmimals.Add(new Horse());
 anmimals.Add(new Wolf());
 anmimals.Add(new Worm());
+anmimals.Add(new Arv.Wolfman());
+
+
+
+
 
 
 foreach (Animal anm in anmimals) { 
@@ -22,7 +26,7 @@ foreach (Animal anm in anmimals) {
     {
         try
         {
-            IPerson person = (IPerson)anm;
+            IPerson person = new Arv.Helper.Wolfman();            
             Console.WriteLine(person.Talk());
         }
         catch (Exception e)
@@ -44,7 +48,6 @@ doglist.Add(new Dog());
 doglist.Add(new Dog());
 doglist.Add(new Dog());
 // 10.F 11.F//doglist.Add(new Horse());// detta går inte för att Häst är inte rätt type för listan.
-
 
 Console.ReadLine(); 
 
